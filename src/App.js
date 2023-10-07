c4c433d7f29ab2c503fa615532556fe9c517017c
Megha <meghadhangar1301@gmail.com>
2023-10-07 15:48:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './index.css'
+import NewListt from './NewListt';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* <Search/> */}
+       {/* <Megha/> */}
+       <NewListt/>
     </div>
   );
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+// import NewsList from './component/NewsList';
+// import SearchBar from './component/SearchBar';
+// import Pagination from './component/Pagination';
+// import './App.css';
+
+
+// function App() {
+//   const [searchQuery, setSearchQuery] = useState('');
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   const totalPages = 10; // Replace with the actual total number of pages
+//   const handlePageChange = (page) => {
+//     setCurrentPage(page);
+//   };
+//   return (
+//     <div className="container">
+//       <h1 className="mb-4">News List</h1>
+//       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+//       <NewsList searchQuery={searchQuery} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+//       <Pagination currentPage={currentPage} totalPages={totalPages} handlePageChange={handlePageChange} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
