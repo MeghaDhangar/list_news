78f41083ffb391774daa730fa4addb8826383709
Megha <meghadhangar1301@gmail.com>
2023-10-09 13:11:32 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -5,41 +5,12 @@ import NewListt from './NewListt';
 function App() {
   return (
     <div className="App">
-       {/* <Search/> */}
-       {/* <Megha/> */}
        <NewListt/>
     </div>
   );
 }
-
 export default App;
 
-// import React, { useState } from 'react';
-// import NewsList from './component/NewsList';
-// import SearchBar from './component/SearchBar';
-// import Pagination from './component/Pagination';
-// import './App.css';
-
-
-// function App() {
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [currentPage, setCurrentPage] = useState(1);
-
-//   const totalPages = 10; // Replace with the actual total number of pages
-//   const handlePageChange = (page) => {
-//     setCurrentPage(page);
-//   };
-//   return (
-//     <div className="container">
-//       <h1 className="mb-4">News List</h1>
-//       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-//       <NewsList searchQuery={searchQuery} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-//       <Pagination currentPage={currentPage} totalPages={totalPages} handlePageChange={handlePageChange} />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 
